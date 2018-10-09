4cc2b0916efb76e7b0affc53d0d90720f2ee288b
alexHooper <alexmohack@gmail.com>
2018-10-09 17:43:49 +0530

    edit the code version

--- nlu_model.py
+++ nlu_model.py
@@ -1,19 +1,19 @@
-from rasa_nlu.converters import load_data
-from rasa_nlu.config import RasaNLUConfig
-from rasa_nlu.model import Trainer
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer, Interpreter
+from rasa_nlu import config
 
 def train_nlu(data, config, model_dir):
-	training_data = load_data(data)
-	trainer = Trainer(RasaNLUConfig(config))
+	training_data = load_data('./data/data.json')
+	trainer = Trainer(config.load("config_spacy.json"))
 	trainer.train(training_data)
+	model_directory = trainer.persist(model_dir, fixed_model_name='nlu', project='current')
 
-	model_directory = trainer.persist(model_dir, fixed_model_name='weathernlu')
 
-
-def run_nlu():
-	interpreter = Interpreter.load('./models/current/nlu', RasaNLUConfig('config_spacy.json'))
-	print(interpreter.parse("Hello"))
+def run_nlu(model_dir):
+	interpreter = Interpreter.load(model_dir)
+	print(interpreter.parse("I am planning a trip to Indore. What's the weather out there."))
 
 
 if __name__ == '__main__':
-	train_nlu('./data/data.json', 'config_spacy.json', './models/nlu')
+	# train_nlu('./data/data.json', 'config_spacy.json', './models')
+	run_nlu('./models/current/nlu')
